73dd045d990aae8d6dd316a07c8f10e28ef9ebfc
João Alberto <joao.alb@usp.br>
2022-07-07 20:56:27 -0300

    Rewritten most things with no funcitonal change

--- tod_plots.py
+++ tod_plots.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1252 -*-
 '''
-Created on 13/07/2020
-Last update 22/02/2022
+Created on 07/2020
+Last update 07/2022
 
 Author: Carlos Otobone, João Alberto
 '''
@@ -13,51 +13,58 @@ import os
 import h5py
 
 
+TOD_FILE_FORMAT = 'bingo_tod_horn_{horn}_{yyyy}{mm}{dd}_{{hh:02d}}0000.h5'
 
-def plot_tod(w_path, date, horn, ti=0, tf=24,title=None): 
+
+def collect_tod_data(path_time_fmt, hi=0, hf=23):
+	
+	tods_matrix = np.array([])
+	times_matrix = np.array([])
+	print("Collecting data from {} TODs...".format(hf-hi))
+	for hh in range(hi,hf+1):
+		arx = path_time_fmt.format(hh=hh)
+		with h5py.File(arx, "r" ) as tod_file:
+			tods = tod_file["P/Phase1"][()]
+			times = tod_file["TIME"][()]
+		tods_matrix = np.hstack((tods_matrix,tods)) if tods_matrix.size else tods
+		times_matrix = np.hstack((times_matrix,times)) if times_matrix.size else times
+	
+	return tods_matrix, times_matrix
+
+
+def date_split(date):
+
+	year = date.split()[0].split('-')[0]
+	month = date.split()[0].split('-')[1]
+	day = date.split()[0].split('-')[2]
+	
+	return year, month, day
+
+
+def plot_tod(tods_path, date, horn, hi=0, hf=23,title=None): 
 	'''
 	Generates the plot for a TOD from hourly divided files, 
 	which names must follow the format:
 	'bingo_tod_horn_{HORN}_{YEAR}{MONTH}{DAY}_{HOUR}0000.h5'
 	
-	w_path: TODs path (only the paths).
+	tods_path: TODs path (only the paths).
 	date: date in format 'yyyy-mm-dd'.
 	horns: horns quantity.
 	'''
 	
-	tods_matrix = []
-	times_matrix = []
-	tods = []
-	times = []
-	
-	year = date.split()[0].split('-')[0]
-	month = date.split()[0].split('-')[1]
-	day = date.split()[0].split('-')[2]
-	path = w_path + year + "/" + month + "/" + day + "/"
+	year, month, day = date_split(date)
+	path = tods_path + year + "/" + month + "/" + day + "/"
 		
-	print("Collecting data from all TODs...")
-	for i in range(tf-ti):
-		t = ti+i
-		arx = 'bingo_tod_horn_' + str(horn) + '_' + year + month + day + '_{:02d}0000.h5'.format(t)				  
-		tod_file = h5py.File( path + arx, "r" )  
-		tods.append(tod_file["P/Phase1"][()])
-		times.append(tod_file["TIME"][()])
-		tod_file.close()
-		tods_matrix.append(tods)
-		times_matrix.append(times)
-	
-	# Concatanating TODs
-	conc_tods = tods_matrix[horn][0]
-	for i in range(1,tf-ti):
-		tod = tods_matrix[horn][i]
-		conc_tods = np.concatenate((conc_tods,tod), axis = 1)
+	file_time_fmt = TOD_FILE_FORMAT.format(horn=horn, yyyy=year, mm=month, dd=day)
+	path_time_fmt = os.path.join(path, file_time_fmt)
+	tods_matrix, times_matrix = collect_tod_data(path_time_fmt, hi, hf)
 
 	print("Creating directory tod_plots...")
 	os.system('mkdir -p ' + path + 'tod_plots/')
 	
 	print ("Generating horn image --> " + str(horn))
-	if title==None: title = "Horn " + str(horn) + " " + year + "/" + month + "/" + day
-	plt.imshow(conc_tods, aspect="auto", origin="lower", extent=(ti,tf,980,981))#,980, 1260))
+	title = "Horn " + str(horn) + " " + year + "/" + month + "/" + day if title==None else title
+	plt.imshow(tods_matrix, aspect="auto", origin="lower", extent=(hi,hf, 980, 1260))
 	plt.title(title)
 	plt.xlabel("Time (h)")
 	plt.ylabel("Frequency (MHz)")	   
@@ -67,129 +74,78 @@ def plot_tod(w_path, date, horn, ti=0, tf=24,title=None):
 	
 
 
-def plot_channel(w_path, date, horn, channel=0, ti=0, tf=24, title=None):
+def plot_channel(tods_path, date, horn, channel=0, hi=0, hf=24, title=None):
 	'''
 	Generates the plot for a specific frequency channel from hourly divided files, 
 	which names must follow the format:
 	'bingo_tod_horn_{HORN}_{YEAR}{MONTH}{DAY}_{HOUR}0000.h5'
 	
-	w_path: TODs path (only the paths).
+	tods_path: TODs path (only the paths).
 	date: date in format 'yyyy-mm-dd'.
 	horns: horns quantity.
 	'''
 	
-	mini = []
-	maxi = []
-	tods_matrix = []
-	times_matrix = []
-	tods = []
-	times = []
+	year, month, day = date_split(date)
+	path = tods_path + year + "/" + month + "/" + day + "/"
 	
-	year = date.split()[0].split('-')[0]
-	month = date.split()[0].split('-')[1]
-	day = date.split()[0].split('-')[2]
-	path = w_path + year + "/" + month + "/" + day + "/"
-		
-	print("Collecting data from all TODs...")
-	for i in range(tf-ti):
-		t = ti+i
-		arx = 'bingo_tod_horn_' + str(horn) + '_' + year + month + day + '_{:02d}0000.h5'.format(t)				  
-		tod_file = h5py.File( path + arx, "r" )  
-		tods.append(tod_file["P/Phase1"][()])
-		times.append(tod_file["TIME"][()])
-		tod_file.close()
-		mini.append(np.min(tods[i]))		
-		maxi.append(np.max(tods[i]))
-		tods_matrix.append(tods)
-		times_matrix.append(times)
-		
-	mini = np.min(mini)
-	maxi = np.max(maxi)			
-	
-	# Concatanating TODs
-	conc_tods = tods_matrix[0][0]
-	for i in range(tf-ti-1):
-		tod = tods_matrix[0][i+1]
-		conc_tods = np.concatenate((conc_tods,tod), axis = 1)
+	file_time_fmt = TOD_FILE_FORMAT.format(horn=horn, yyyy=year, mm=month, dd=day)
+	path_time_fmt = os.path.join(path, file_time_fmt)
+	tods_matrix, times_matrix = collect_tod_data(path_time_fmt, hi, hf)	
 
 	print("Creating directory tod_plots...")
 	os.system('mkdir -p ' + path + 'tod_plots/')
-	if title==None: 
-		title = ("Horn " + str(horn) + " Channel " + str(channel) + " " + 
-				 year + "/" + month + "/" + day)
+	title = "Horn {horn} Channel {channel} {year}/{month}/{day}".format(horn=horn,
+																		channel=channel,
+																		year=year,
+																		month=month,
+																		day=day) \
+															if title==None else title
 	
 	temp_plot = True
 	db_plot = False
 	print ("Generating horn image --> " + str(horn))
 	if temp_plot:
-		plt.plot(np.linspace(ti,tf,len(conc_tods[channel])), 
-				 conc_tods[channel])
+		seconds = np.linspace(hi,hf,len(tods_matrix[channel]))
+		plt.plot(seconds, tods_matrix[channel])
 		plt.title(title)
 		plt.xlabel("Time (h)")
-		plt.xlim(5.1,5.5)
+		#plt.xlim(5.1,5.5)
 		plt.ylabel("Temperature (K)")
 		plt.savefig(path + "tod_plots/" + '24_bingo_tod_horn_' + str(horn) + 
 					'_channel_' + str(channel) + '_' + year + month + day + ".png")
 		plt.close()
 		
 	if db_plot:
-		plt.plot(np.linspace(ti,tf,len(conc_tods[channel])),
-				 20*np.log10(abs(conc_tods[channel])))
+		seconds = np.linspace(hi,hf,len(tods_matrix[channel]))
+		plt.plot(seconds, 20*np.log10(abs(tods_matrix[channel])))
 		plt.title(title)
 		plt.xlabel("Time (h)")
-		plt.xlim(5.1,5.5)
+		#plt.xlim(5.1,5.5)
 		plt.ylabel("Amplitude (dB)")
 		plt.savefig(path + "tod_plots/" + '24_bingo_tod_amplitude_horn_' + str(horn) + 
 					'_channel_' + str(channel) + '_' + year + month + day + ".png")
 		plt.close()
 
 
-def plot_diff(tod_path_1, tod_path_2, date, horn, output_file, title):
+def plot_diff(tod_path_1, tod_path_2, date, horn, output_file, title, hi=0, hf=23):
 	'''
 	Plots the difference between two TODs for a specific date.
 	'''
-	year = date.split()[0].split('-')[0]
-	month = date.split()[0].split('-')[1]
-	day = date.split()[0].split('-')[2]
+	
+	year, month, day = date_split(date)
 	path1 = tod_path_1 + year + "/" + month + "/" + day + "/"
 	path2 = tod_path_2 + year + "/" + month + "/" + day + "/"
 	
-	tods_matrix = []
-	times_matrix = []
-	tods = []
-	times = []
-	print("Collecting data from all TODs...")
-	for i in range(24):
-		arx = 'bingo_tod_horn_' + str(horn) + '_' + year + month + day + '_{:02d}0000.h5'.format(i)
-		with h5py.File(path1 + arx, "r" ) as tod_file:			
-			tods.append(tod_file["P/Phase1"][()])
-			times.append(tod_file["TIME"][()])
-		tods_matrix.append(tods)
-		times_matrix.append(times)
-		
-	tod_1 = tods_matrix[horn][0]
-	for i in range(23):
-		tod = tods_matrix[horn][i+1]
-		tod_1 = np.concatenate((tod_1,tod), axis=1)
-		
-	tods_matrix = []
-	times_matrix = []
-	tods = []
-	times = []
-	for i in range(24):
-		arx = 'bingo_tod_horn_' + str(horn) + '_' + year + month + day + '_{:02d}0000.h5'.format(i)
-		with h5py.File(path2 + arx, "r" ) as tod_file:			
-			tods.append(tod_file["P/Phase1"][()])
-			times.append(tod_file["TIME"][()])
-		tods_matrix.append(tods)
-		times_matrix.append(times)  
-			  
-	tod_2 = tods_matrix[horn][0]
-	for i in range(23):
-		tod = tods_matrix[horn][i+1]
-		tod_2= np.concatenate((tod_2,tod), axis=1)
+	file_time_fmt = TOD_FILE_FORMAT.format(horn=horn, yyyy=year, mm=month, dd=day)
+	#file_time_fmt2 = TOD_FILE_FORMAT.format(horn=horn, yyyy=year, mm=month, dd=day)
 	
-	tod_diff = tod_1 - tod_2
+	path_time_fmt1 = os.path.join(path1, file_time_fmt)
+	tods_matrix1, times_matrix1 = collect_tod_data(path_time_fmt1, hi, hf)
+
+	path_time_fmt2 = os.path.join(path, file_time_fmt)
+	tods_matrix2, times_matrix2 = collect_tod_data(path_time_fmt2, hi, hf)
+	
+	tod_diff = tods_matrix1 - tods_matrix2
 	
 	print ("Generating image...")
 	plt.imshow(tod_diff, aspect="auto", origin="lower", extent=(0,24 ,980, 1260))
@@ -286,24 +242,8 @@ if __name__=="__main__":
 	date = '2018-01-01'
 	horn = 0
 	
-	tod_path = '/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/normalization_tests/nside512/zernike_cubic_cut/'
-	#plot_channel(tod_path, date, horn, ti=5,tf=6)
+	tod_path = "/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/teste/"
+	plot_channel(tod_path, date, horn, hi=5,hf=6)
 	
-	paths = ["/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/normalization_tests/nside{}/gaussian_cut_norm/",
-			 "/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/normalization_tests/nside{}/gaussian_cut_not_norm/",
-			 "/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/normalization_tests/nside{}/zernike_cubic_cut/",
-			 "/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/normalization_tests/nside{}/zernike_linear_cut/",
-			 "/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/normalization_tests/nside{}/zernike_nearest_cut/"]
-	for tod_path in paths:
-		plot_tod(tod_path.format(256), date, horn, ti=5, tf=6)
-		plot_channel(tod_path.format(256), date, horn, ti=5, tf=6)
 
-#	path1 = "/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/ame_zernike/zernike_30_MINUS304_nside512_nearest/"
-#	path2 = "/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/ame_zernike/gaussian_512/"
-#	date = "2018-01-01"
-#	horn = 0
-#	output_file = "/home/joao/Documentos/cosmologia/hide_and_seek/resultados/TOD/ame_zernike/tod_sym_beam_gaussian_difference.png"
-#	title = "(30, -304) - Gauss , nside512, nearest"
-#	
-#	plot_diff(path1, path2, date, horn, output_file, title)
 	
